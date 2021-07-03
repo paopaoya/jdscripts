0a96b014da07fe1db087003266e6ded540c375ce
paopaoya <75239531+paopaoya@users.noreply.github.com>
2021-07-03 15:18:03 +0800

    Update jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -6,7 +6,10 @@
 //云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
-let shareCodes = []
+let shareCodes = [
+  '2FxlKcyWr_nSrRk-00USKw==@x2ukrVHolUDJPv26rXQIRg==@nxuaKRCOTTNWHuF1VPoWgQ==',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '2FxlKcyWr_nSrRk-00USKw==@x2ukrVHolUDJPv26rXQIRg==@nxuaKRCOTTNWHuF1VPoWgQ==',//账号二的好友shareCode，不同好友中间用@符号隔开
+]
 
 // 从日志获取互助码
 // const logShareCodes = require('./utils/jdShareCodes');
