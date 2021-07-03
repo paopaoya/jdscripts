32d928853c0432d18f9e2d7242a963ce9a6d2d1e
paopaoya <75239531+paopaoya@users.noreply.github.com>
2021-07-03 15:24:56 +0800

    Update jdPlantBeanShareCodes.js

--- jdPlantBeanShareCodes.js
+++ jdPlantBeanShareCodes.js
@@ -6,7 +6,10 @@
 //云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
-let PlantBeanShareCodes = []
+let PlantBeanShareCodes = [
+  '6mfbm55vnxch3src36weenvxlu@mlrdw3aw26j3xkw47m2k3z7ppr7xkqfjrtdcj7a@u72q4vdn3zes3pcmwyukpldulioikl2h7ysa2uy',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '6mfbm55vnxch3src36weenvxlu@mlrdw3aw26j3xkw47m2k3z7ppr7xkqfjrtdcj7a@u72q4vdn3zes3pcmwyukpldulioikl2h7ysa2uy',//账号二的好友shareCode，不同好友中间用@符号隔开
+]
 
 // 从日志获取互助码
 // const logShareCodes = require('./utils/jdShareCodes');
