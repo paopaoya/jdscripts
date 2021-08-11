f2d1ebf0f58647bec566a4e1d84d6c8f0fa6bac9
paopaoya <75239531+paopaoya@users.noreply.github.com>
2021-08-11 16:13:47 +0800

    fix

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -20,6 +20,8 @@
 35 1,22 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 # 5G超级盲盒(活动时间：2021-8-2到2021-10-29)
 33 0,1-23/3 * * * node /scripts/jd_mohe.js >> /scripts/logs/jd_mohe.log 2>&1
+# 超级直播间红包雨(活动时间不定期，出现异常提示请忽略。红包雨期间会正常)
+1,31 0-23/1 * * * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,9 * * * node /scripts/jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
